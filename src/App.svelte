243be9a7ad7c70468e72791bd9af758bcf32c6fa
henrikmathiesen <henrik_mathiesen@hotmail.com>
2026-07-18 17:25:26 +0200

    Design.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 
 import JumboHeader from "./lib/components/JumboHeader.svelte";
+import NewsItem from "./lib/components/NewsItem.svelte";
 </script>
 
-<div class="container">
+<div class="container container--main">
     <div class="row">
         <div class="col-12">
             <JumboHeader></JumboHeader>
@@ -39,9 +40,11 @@ import JumboHeader from "./lib/components/JumboHeader.svelte";
         </aside>
 
         <aside class="col-12 col-md-6 col-lg-3">
-            <hr>
-            NEWS
-            <hr>
+            <NewsItem></NewsItem>
+            <NewsItem></NewsItem>
+            <NewsItem></NewsItem>
+            <NewsItem></NewsItem>
+            <NewsItem></NewsItem>
         </aside>
     </div>
 </div>
