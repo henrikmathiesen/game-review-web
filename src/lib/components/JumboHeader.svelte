0791081d6633a13bd0a7fe88e9ba1125c6fefdea
henrikmathiesen <henrik_mathiesen@hotmail.com>
2026-07-26 16:42:30 +0200

    Login button

--- src/lib/components/JumboHeader.svelte
+++ src/lib/components/JumboHeader.svelte
@@ -1,4 +1,12 @@
+<script lang="ts">
+    import Button from './Button.svelte';
+</script>
+
 <header class="jumbo-header">
+    <div class="jumbo-header__login">
+        <Button semantic="warning" label="Login" />
+    </div>
+
     <div class="jumbo-header__content">
         <h1 class="sr-only">Gaming is the Opium - Games and Reviews</h1>
     </div>
@@ -6,8 +14,16 @@
 
 <style>
     .jumbo-header {
+        position: relative;
         display: flex;
         justify-content: end;
+    }
+
+    .jumbo-header__login {
+        position: absolute;
+        inset-block-start: 1rem;
+        inset-inline-start: 0;
+        z-index: 1;
     }
 
     .jumbo-header__content {
