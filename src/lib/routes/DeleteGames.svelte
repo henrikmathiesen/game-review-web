9360def9d95d161aa754a74c1a1b3378bee0b799
henrikmathiesen <henrik_mathiesen@hotmail.com>
2026-08-20 19:39:18 +0200

    View skeleton code

--- src/lib/routes/DeleteGames.svelte
+++ src/lib/routes/DeleteGames.svelte
@@ -1,8 +1,28 @@
+<script lang="ts">
+    import { gameRepository } from "../config/repositories";
+    import Banner from "../components/Banner.svelte";
+    import LoadingAnimation from "../components/graphics/LoadingAnimation.svelte";
+    import GameList from "../components/game/GameList.svelte";
+
+    const gamesPromise = gameRepository.getAll();
+</script>
+
 <main class="row">
     <div class="col-12 col-md-6">
-
+        <Banner semantic="warning" heading="Important">
+            <p>
+                Check the games you want to delete and click the delete button.
+                The reviews for each game will be deleted as well. The action is irreversible.
+            </p>
+        </Banner>
     </div>
     <div class="col-12 col-md-6">
-
+        {#await gamesPromise}
+            <LoadingAnimation />
+        {:then games}
+            <GameList games={games}></GameList>
+        {:catch error}
+            <p>Could not load games</p>
+        {/await}
     </div>
 </main>
--- src/lib/routes/DeleteReviews.svelte
+++ src/lib/routes/DeleteReviews.svelte
@@ -1,8 +1,28 @@
+<script lang="ts">
+    import { reviewRepository } from "../config/repositories";
+    import Banner from "../components/Banner.svelte";
+    import LoadingAnimation from "../components/graphics/LoadingAnimation.svelte";
+    import ReviewList from "../components/review/ReviewList.svelte";
+
+    const reviewsPromise = reviewRepository.getAll();
+</script>
+
 <main class="row">
     <div class="col-12 col-md-6">
-
+        <Banner semantic="warning" heading="Important">
+            <p>
+                Check the reviews you want to delete and click the delete button.
+                The action is irreversible.
+            </p>
+        </Banner>
     </div>
     <div class="col-12 col-md-6">
-
+        {#await reviewsPromise}
+            <LoadingAnimation />
+        {:then reviews}
+            <ReviewList {reviews} />
+        {:catch error}
+            <p>Could not load reviews</p>
+        {/await}
     </div>
 </main>
