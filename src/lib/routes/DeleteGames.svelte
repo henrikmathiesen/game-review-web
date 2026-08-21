487a090a65b3cc8b5fd1abab1e56c51343a877c8
henrikmathiesen <henrik_mathiesen@hotmail.com>
2026-08-21 18:58:54 +0200

    Form

--- src/lib/routes/DeleteGames.svelte
+++ src/lib/routes/DeleteGames.svelte
@@ -3,26 +3,32 @@
     import Banner from "../components/Banner.svelte";
     import LoadingAnimation from "../components/graphics/LoadingAnimation.svelte";
     import GameList from "../components/game/GameList.svelte";
+    import Button from "../components/Button.svelte";
 
     const gamesPromise = gameRepository.getAll();
 </script>
 
-<main class="row">
-    <div class="col-12 col-md-6">
-        <Banner semantic="warning" heading="Important">
-            <p>
-                Check the games you want to delete and click the delete button.
-                The reviews for each game will be deleted as well. The action is irreversible.
-            </p>
-        </Banner>
-    </div>
-    <div class="col-12 col-md-6">
-        {#await gamesPromise}
-            <LoadingAnimation />
-        {:then games}
-            <GameList games={games}></GameList>
-        {:catch error}
-            <p>Could not load games</p>
-        {/await}
-    </div>
+<main>
+    <form class="row">
+        <div class="col-12 col-md-6">
+            <Banner semantic="warning" heading="Important">
+                <p>
+                    Check the games you want to delete and click the delete button.
+                    The reviews for each game will be deleted as well. The action is irreversible.
+                </p>
+            </Banner>
+            <div class="spacing-row-t ">
+                <Button semantic="danger" label="Delete selected games" buttonType="submit"></Button>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            {#await gamesPromise}
+                <LoadingAnimation />
+            {:then games}
+                <GameList games={games}></GameList>
+            {:catch error}
+                <p>Could not load games</p>
+            {/await}
+        </div>
+    </form>
 </main>
